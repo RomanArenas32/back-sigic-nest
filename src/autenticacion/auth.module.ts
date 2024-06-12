b1ea692f153dabba6f1f8b2d2817031b5885b761
Roman Arenas <roman.ea3232@gmail.com>
2024-06-12 16:18:48 -0300

    autenticacion_token

--- src/autenticacion/auth.module.ts
+++ src/autenticacion/auth.module.ts
@@ -3,12 +3,19 @@ import { UsuariosService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuariosController } from './auth.controller';
 import { Usuario } from 'src/usuarios/entities/usuario.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants/jwt-constant';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       Usuario
-    ])
+    ]),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '3h' },
+    }),
   ],
   controllers: [UsuariosController],
   providers: [UsuariosService]
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  // Configuración de CORS
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://papaya-beignet-2efb70.netlify.app'], 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
   });
-  await app.listen(3000);
+
+  await app.listen(3000, () => {
+    console.log('Servidor NestJS corriendo en el puerto 3000');
+  });
 }
 bootstrap();
